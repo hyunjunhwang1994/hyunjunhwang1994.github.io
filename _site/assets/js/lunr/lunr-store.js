09c876631bc4cb911da1242e48434c304adf47fb
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2022-11-20 19:58:52 +0900

    ㅜ

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "맥북 M1, VS code Paste Image",
+        "excerpt":"VS code에서 MarkDown 이미지 쉽게 붙혀넣기. Paste Image란? 이미지 복사 -&gt; VS code 마크다운 문서 -&gt; Command + Option + V -&gt; 이미지 파일 자동 저장및, 문서에 삽입 VS Code의 확장에 들어오면 Paste Image라는 앱이 존재 합니다. 설치 설정. 1. Paste Image: Path 실제 이미지 파일이 저장될 경로에 대한 옵션입니다....","categories": [],
+        "tags": [],
+        "url": "/first/",
+        "teaser": null
+      },{
+        "title": "맥북 M1 깃허브 지킬 블로그 만들기 총 정리 1탄",
+        "excerpt":"시작 전, 깃허브 계정을 만들어주세요. repositories에 들어가서 자신의 블로그로 사용할 레파지토리를 만듭니다. Repositroy name은 꼭 깃허브계정.github.io로 만들어주세요 ( 본인의 블로그 주소입니다. ) 다른 지킬 테마를 fork로 가저오면서 레파지토리를 만들면 편하지만, 그럴 경우 커밋과 같은 과정에서 잔디가 심어지지 않아요ㅜㅜ 그렇기 때문에 이와 같이 직접 테마를 넣어줘야 합니다. 일단 깃허브 데스크탑 깔아주세요...","categories": [],
+        "tags": [],
+        "url": "/jekyll/",
+        "teaser": null
+      },{
+        "title": "맥북 M1 깃허브 지킬 블로그 만들기 총 정리 2탄",
+        "excerpt":"기본적인 포스팅 방법 포스팅 할 글의 경우 /posts 에 올려주면 됩니다. 하지만 처음에 없으므로 생성합니다. 새로운 파일생성 _posts/ &lt;- / 를 치면 폴더 생성입니다. 파일 제목은 꼭 년도-월-일-ab-cd.md 형식으로 만들어야합니다. 그후 커밋 뉴 파일을 하면 ? 이와같은식으로 글이 잘 올라오는것을 볼 수 있습니다. 그런데 커밋후 글이올라오는데 시간이 좀 걸리네요! 시간...","categories": [],
+        "tags": [],
+        "url": "/jekyll2/",
+        "teaser": null
+      },{
+        "title": "맥북 M1 깃허브 지킬 블로그 만들기 총 정리 3탄",
+        "excerpt":"이번에는 터미널 활용과, 로컬서버 생성을 하기 위한 셋업등을 알려드릴게요. 결론적으로 아래와 같은 것들을 설치해야 합니다. Homebrew rbenv ruby (맥은 기본적으로 깔려있지만, 다른버전 설치해야함) jekyll, bundler Homebrew https://brew.sh/index_ko 접속 후 설치. 맥 터미널에서 /bin/bash -c “$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)” 위와 같은 명령어 실행 시 다운로드 할 수 있다. 이때 혹시 아래와 같은...","categories": [],
+        "tags": [],
+        "url": "/jekyll3/",
+        "teaser": null
+      }]
