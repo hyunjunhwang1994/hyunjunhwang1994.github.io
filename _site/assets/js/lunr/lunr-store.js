eff62e3b08444546afecceb01ef4c6fddf3fd8a6
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2022-12-01 09:17:57 +0900

    update posting

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,120 +2,120 @@ var store = [{
         "title": "맥북 M1 JDK 1.8 (Java 8) 설치하는 법",
         "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 라이선스 비용 및 안정성 등 여러 가지 이유로 현업에서는 1.8 버전을 많이 사용합니다. Oracle JDK의 경우 M1, ARM 방식의 JDK 8버전은 지원하지 않으므로.. Open JDK인 Azul Zulu를 받아야 한다. Open JDK 다운로드 된 설치 파일...","categories": ["Basic of java"],
         "tags": ["맥북 M1 JDK 1.8 설치","M1 JDK 환경변수 설정"],
-        "url": "/basic%20of%20java/Java0/",
+        "url": "/basic%20of%20java/BasicOfJava1/",
         "teaser": null
       },{
         "title": "맥북 M1 이클립스 설치하는 법",
         "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 버전 JDK: openjdk version 1.8.0_352 Eclipse: Version: 2022-09 (4.25.0) JDK 1.8 설치 과정은 아래 링크 참조해 주세요. JDK 1.8 설치 이클립스 설치. 문제점. JDK 1.8 ( ARM ), 최신 이클립스 ( ARM ) 돌리면 이클립스는...","categories": ["Basic of java"],
         "tags": ["맥북 M1 JDK 1.8 이크립스 설치","M1 JDK 이클립스 설치"],
-        "url": "/basic%20of%20java/Java1/",
+        "url": "/basic%20of%20java/BasicOfJava2/",
         "teaser": null
       },{
         "title": "이클립스 내 JDK 버전 변경, JDK 1.8 버전 변경 에러",
         "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 버전 JDK: openjdk version 1.8.0_352 Eclipse: Version: 2022-09 (4.25.0) JDK 버전 변경 시 JDK 설치와 환경 변수 적용 등이 미리 되어 있어야 합니다. JDK 설치 참조 https://jshwang94.tistory.com/3 이클립스 설치 참조 https://jshwang94.tistory.com/4 프로젝트 JDK 버전 확인...","categories": ["Basic of java"],
         "tags": ["이클립스 JDK 버전 변경","Syntax error on token module module expected error"],
-        "url": "/basic%20of%20java/java2/",
-        "teaser": null
-      },{
-        "title": "개발자, 깃허브 블로그 포트폴리오 만들기(맥북 M1)",
-        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 깃허브 깃허브 계정 만들기. 아래의 링크에서 깃허브 계정을 만든다. 깃허브 Repository(저장소) 만들기 Repository에 들어가서 자신의 블로그로 사용 할 레퍼지토리를 만들어야 한다. 우상단 프로필클릭 -&gt; Your repositories -&gt; New를 눌러 새 레파지토리 생성 Repository name은 꼭...","categories": ["블로그 만들기"],
-        "tags": ["Github blog","개발자 블로그 포트폴리오","Jekyll blog","Minimal Mistakes 설치"],
-        "url": "/%EB%B8%94%EB%A1%9C%EA%B7%B8%20%EB%A7%8C%EB%93%A4%EA%B8%B0/jekyll1/",
-        "teaser": null
-      },{
-        "title": "포스팅 하는법",
-        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 기본적인 포스팅 방법 포스팅할 글의 경우 /posts에 올려주면 됩니다. 하지만 처음에 없으므로 생성합니다. 새로운 파일 생성 _posts/ &lt;- / 를 치면 폴더 생성입니다. 파일 제목은 꼭 년도-월-일-ab-cd.md 형식으로 만들어야 합니다. Commit new file을 해주세요. 이와...","categories": ["블로그 만들기"],
-        "tags": ["Github blog 포스팅","Jekyll 포스팅 하는 법"],
-        "url": "/%EB%B8%94%EB%A1%9C%EA%B7%B8%20%EB%A7%8C%EB%93%A4%EA%B8%B0/jekyll2/",
-        "teaser": null
-      },{
-        "title": "지킬 블로그 로컬 환경 만들기",
-        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 설치 목록. 이번에는 터미널 활용과, 로컬 서버 생성을 하기 위한 셋업 등을 알려드릴게요. 결론적으로 아래와 같은 것들을 설치해야 합니다. Homebrew rbenv ruby (맥은 기본적으로 깔려있지만, 다른 버전 설치해야 함) jekyll, bundler Homebrew 설치 아래 링크에서...","categories": ["블로그 만들기"],
-        "tags": ["Jekyll blog 로컬 환경 만들기","homebrew 설치","rbenv 설치","루비 설치","Jekyll과 Bundler 설치"],
-        "url": "/%EB%B8%94%EB%A1%9C%EA%B7%B8%20%EB%A7%8C%EB%93%A4%EA%B8%B0/jekyll3/",
-        "teaser": null
-      },{
-        "title": "깃허브 블로그 로컬 호스팅 하기 (실시간 확인)",
-        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 블로그 로컬 호스팅 하기 자신의 블로그 레파지토리 폴더를 터미널로 연다. 해당 폴더 안에서 아래의 명령어 입력으로 번들을 설치해 준다. bundle install 그럼 번들이 우리의 블로그에 설치되었을 것이다. 그렇다면 아래의 명령어로 서버를 실행시켜준다. bundle exec jekyll...","categories": ["블로그 만들기"],
-        "tags": ["Jekyll blog 로컬 환경 만들기","Jekyll blog 실시간 확인"],
-        "url": "/%EB%B8%94%EB%A1%9C%EA%B7%B8%20%EB%A7%8C%EB%93%A4%EA%B8%B0/jekyll4/",
-        "teaser": null
-      },{
-        "title": "VS Code 설치, 확장팩 설치",
-        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 포스팅을 편리하게 하기 위한 VS code 설치 설치 아래의 링크에서 설치해 주세요. VS Code 익스텐션 설치 확장팩을 설치하는 과정입니다. 왼쪽 맨 아래 확장 메뉴에서 해당 팩들을 설치해 주세요. (저는 설치되어 있어서 제거로 나오고 있네요.) 한글...","categories": ["블로그 만들기"],
-        "tags": ["VS code 설치","Paste image 설치","VS code 한글 패치"],
-        "url": "/%EB%B8%94%EB%A1%9C%EA%B7%B8%20%EB%A7%8C%EB%93%A4%EA%B8%B0/jekyll5/",
-        "teaser": null
-      },{
-        "title": "매우 편리한 이미지 넣기 VS code Paste Image",
-        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 Paste Image? 공식 움짤인데..? 참 설명도 헷갈리게 해 놓았다.. Paste Image 앱은 VS Code 내에서 돌아가며, 인터넷상 존재하는 혹은 캡처한 이미지 파일을 VS Code에서 간단하게 저장 및 Markdown 자동 작성까지 해준다. (여기서 포인트는 문법 작성뿐...","categories": ["블로그 만들기"],
-        "tags": ["Paste image 사용법"],
-        "url": "/%EB%B8%94%EB%A1%9C%EA%B7%B8%20%EB%A7%8C%EB%93%A4%EA%B8%B0/jekyll6/",
-        "teaser": null
-      },{
-        "title": "_config,yml 수정, 블로그 정보 수정하기",
-        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 _config.yml? _config.yml은 Jekyll 블로그 생성시 만들어지는 파일로 여러가지 블로그 관련 정보를 설정 할 수 있다. 스킨 선택 minimal_mistakes_skin : 설정 하고싶은 스킨을 선택해 보자. Mint를 선택 할 경우 언어 변경 locale : “ko-KR” 할 경우...","categories": ["블로그 만들기"],
-        "tags": ["_config.yml 설정하기"],
-        "url": "/%EB%B8%94%EB%A1%9C%EA%B7%B8%20%EB%A7%8C%EB%93%A4%EA%B8%B0/jekyll7/",
-        "teaser": null
-      },{
-        "title": "깃헙블로그 참조링크, 설정파일 설명 정리",
-        "excerpt":"사이드바크기및 넓이, 하이퍼텍스트 밑줄제거 https://eona1301.github.io/github_blog/GithubBlog-Content-Width/ 똑같은 테마에 전반적인 내용 https://www.youtube.com/watch?v=–MMmHbSH9k 화이트 / 다크테마 토글 적용하기 (Utterances자동색상변경) https://etch-cure.github.io/blog/toggle-dark-mode/ 스크롤바 꾸미기, 설정시 사이드바에도 스크롤바 생겨서 비추 https://ansohxxn.github.io/blog/scrollbar/ 카테고리 만들기 https://ansohxxn.github.io/blog/category/ 카테고리 글안에 다음 이전 넣기. https://ansohxxn.github.io/blog/prevnext/ _data/ui-text.yml 영어 -&gt; 한글 번역될때 실제 한글을 지정가능한 설정파일 아래의 경우 접기안에 들어간내용은 TOC 클릭시 오류가나므로...","categories": ["기능 구현하기"],
-        "tags": ["github","blog"],
-        "url": "/%EA%B8%B0%EB%8A%A5%20%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0/jekyll10/",
-        "teaser": null
-      },{
-        "title": "깃허브 블로그 Disqus 댓글 기능 만들기",
-        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 Disqus Disqus란 댓글 API를 제공하는 회사이다. 보통 네이버나, 티스토리 등 블로그에는 댓글 기능이 달려 있지만, Jekyll 기반 블로그, 개인 블로그 등에 댓글 기능을 추가할 때 사용하면 된다. 회원 가입. 아래 페이지에서 회원 가입을 해주세요. Disqus...","categories": ["기능 구현하기"],
-        "tags": ["Disqus","Jekyll 블로그 댓글 기능"],
-        "url": "/%EA%B8%B0%EB%8A%A5%20%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0/jekyll8/",
-        "teaser": null
-      },{
-        "title": "Google analytics 추가하기",
-        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 Google analytics ? 웹사이트 방문자 데이터를 수집, 분석할 수 있게 도와주는 앱이다. _config.yml 설정하기. Google analytics 첫 사용자인 경우 측정시작 클릭 후 진행. 계정 이름, 속성 이름 설정 후 다음. 업종의 경우 자신이 원하는걸로 선택....","categories": ["기능 구현하기"],
-        "tags": ["Disqus","Jekyll 블로그 댓글 기능"],
-        "url": "/%EA%B8%B0%EB%8A%A5%20%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0/jekyll9/",
-        "teaser": null
-      },{
-        "title": "minimal-mistakes 기반 Utterances 댓글 적용",
-        "excerpt":"Disqus에서 Ufferances로 바꾼 이유. Disqus의 치명적인 단점. 어느샌가 몰래 덕지덕지 붙어있는 광고들.. 왠지 모르게 무겁다. 왜 나만 글자가 투명색(흰색)으로 나오는지ㅠ Disqus 내 설정을 바꿔도, Only 흰색 글자만 나온다. 결국 이러한 점 때문에 바꾸었다. (하얀색 배경으로 된 테마를 사용 못 하기 때문.) 광고 없애려면 유료… 그렇다면 장점은 없나? 구글, 페이스북, 트위터,...","categories": ["기능 구현하기"],
-        "tags": ["Utterances","댓글 기능"],
-        "url": "/%EA%B8%B0%EB%8A%A5%20%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0/utterancesComments/",
-        "teaser": null
-      },{
-        "title": "객체지향 프로그래밍과 절차적 프로그래밍",
-        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 버전 JDK: OpenJDK Runtime Environment (Zulu 8.66.0.15-CA-macos-aarch64) (build 1.8.0_352-b08) IntelliJ: IntelliJ IDEA 2022.2.3 (Community Edition) 절차적 프로그래밍과 객체지향 프로그래밍. 절차적 프로그래밍 ‘절차적 프로그래밍’이라는 한국어 번역은 오해의 여지가 크다. Procedural Programming에서 Procedural를 ‘절차적’으로 번역해버려서 마치 절차적으로...","categories": ["Basic of java"],
-        "tags": ["자바의 객체지향","객체지향 프로그래밍","절차적 프로그래밍"],
-        "url": "/basic%20of%20java/Java3/",
-        "teaser": null
-      },{
-        "title": "객체, 클래스, 인스턴스",
-        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 버전 JDK: OpenJDK Runtime Environment (Zulu 8.66.0.15-CA-macos-aarch64) (build 1.8.0_352-b08) IntelliJ: IntelliJ IDEA 2022.2.3 (Community Edition) 클래스, 객체 상속과 오버라이딩 오버로딩 추상클래스와 인터페이스 패키지와 제어자 객체지향 프로그래밍? ex) 자동차(프로그램)은 바퀴(객체), 엔진(객체), 핸들(객체)등을 조합하여 만든다. 그러한 바퀴,...","categories": ["Basic of java"],
-        "tags": ["객체","클래스","인스턴스"],
-        "url": "/basic%20of%20java/Java4/",
-        "teaser": null
-      },{
-        "title": "스프링 기초",
-        "excerpt":"            개발 환경            - 2021, 맥북 프로 M1 Pro 14인치 모델             - Ventura 13.1 베타(22C5050e) 버전                버전            JDK: OpenJDK Runtime Environment (Zulu 8.66.0.15-CA-macos-aarch64) (build 1.8.0_352-b08)            IntelliJ: IntelliJ IDEA 2022.2.3 (Community Edition)    ","categories": ["Spring"],
-        "tags": ["Spring 기초"],
-        "url": "/spring/Spring/",
-        "teaser": null
-      },{
-        "title": "맥북 이클립스 자동완성, Contents Assist 사용하기",
-        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 버전 JDK: OpenJDK Runtime Environment (Zulu 8.66.0.15-CA-macos-aarch64) (build 1.8.0_352-b08) IntelliJ: IntelliJ IDEA 2022.2.3 (Community Edition) Mac에서 이클립스 자동완성으로 사용하는 단축키 Control + Space는 맥에서 이미 한글/영어변환 키에 사용되고 있다. 맥 단축키를 변경하거나, 이클립스에서 단축키를 변경하면된다....","categories": ["Basic of java"],
-        "tags": ["이클립스","이클립스 자동완성 맥북","맥북 Contents Assist"],
-        "url": "/basic%20of%20java/Java5/",
-        "teaser": null
-      },{
-        "title": "Java 프로그래밍이란?, Hellow Java",
-        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 버전 JDK: openjdk version 1.8.0_352 Eclipse: Version: 2022-09 (4.25.0) 해당 포스팅의 전반적인 내용 및 사진은 인프런의 자바 강의에서 가지고 왔습니다. https://www.inflearn.com/course/%EC%8B%A4%EC%A0%84-%EC%9E%90%EB%B0%94_java-renew/dashboard 프로그래밍이란? 각 나라들의 언어가 다른 것처럼 우리가 생성한 코드(인간의 언어)는 컴파일러를 통해 컴퓨터의 언어로...","categories": ["인프런 실전 자바"],
-        "tags": ["프로그래밍이란?","Java 언어의 탄생","Java 언어의 특징","JDK","JRE"],
-        "url": "/%EC%9D%B8%ED%94%84%EB%9F%B0%20%EC%8B%A4%EC%A0%84%20%EC%9E%90%EB%B0%94/%EC%9D%B8%ED%94%84%EB%9F%B0-%EC%8B%A4%EC%A0%84-%EC%9E%90%EB%B0%94-1/",
+        "url": "/basic%20of%20java/BasicOfJava3/",
         "teaser": null
       },{
         "title": "맥에서 이클립스 오류나는 경우.",
         "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 버전 JDK: OpenJDK Runtime Environment (Zulu 8.66.0.15-CA-macos-aarch64) (build 1.8.0_352-b08) IntelliJ: IntelliJ IDEA 2022.2.3 (Community Edition) 맥에서 아래와 같은 오류나는 경우?? 이클립스 워크스페이스 폴더 경로에 맥에서 사용하는 특수문자가 들어가 있다면 지워 주면 잘 실행 될 것이다....","categories": ["Basic of java"],
         "tags": ["Error: A JNI error has occurred, please check your installation and try again","Exception in thread main java.lang.IllegalArgumentException: Error decoding percent encoded characters"],
-        "url": "/basic%20of%20java/Java6/",
+        "url": "/basic%20of%20java/BasicOfJava4/",
+        "teaser": null
+      },{
+        "title": "맥북 이클립스 자동완성, Contents Assist 사용하기",
+        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 버전 JDK: OpenJDK Runtime Environment (Zulu 8.66.0.15-CA-macos-aarch64) (build 1.8.0_352-b08) IntelliJ: IntelliJ IDEA 2022.2.3 (Community Edition) Mac에서 이클립스 자동완성으로 사용하는 단축키 Control + Space는 맥에서 이미 한글/영어변환 키에 사용되고 있다. 맥 단축키를 변경하거나, 이클립스에서 단축키를 변경하면된다....","categories": ["Basic of java"],
+        "tags": ["이클립스","이클립스 자동완성 맥북","맥북 Contents Assist"],
+        "url": "/basic%20of%20java/BasicOfJava5/",
+        "teaser": null
+      },{
+        "title": "Java 프로그래밍이란?, Hellow Java",
+        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 버전 JDK: openjdk version 1.8.0_352 Eclipse: Version: 2022-09 (4.25.0) 해당 포스팅의 전반적인 내용 및 사진은 인프런의 자바 강의에서 가지고 왔습니다. https://www.inflearn.com/course/%EC%8B%A4%EC%A0%84-%EC%9E%90%EB%B0%94_java-renew/dashboard 프로그래밍이란? 각 나라들의 언어가 다른 것처럼 우리가 생성한 코드(인간의 언어)는 컴파일러를 통해 컴퓨터의 언어로...","categories": ["Basic of java"],
+        "tags": ["프로그래밍이란?","Java 언어의 탄생","Java 언어의 특징","JDK","JRE"],
+        "url": "/basic%20of%20java/BasicOfJava6/",
+        "teaser": null
+      },{
+        "title": "개발자, 깃허브 블로그 포트폴리오 만들기(맥북 M1)",
+        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 깃허브 깃허브 계정 만들기. 아래의 링크에서 깃허브 계정을 만든다. 깃허브 Repository(저장소) 만들기 Repository에 들어가서 자신의 블로그로 사용 할 레퍼지토리를 만들어야 한다. 우상단 프로필클릭 -&gt; Your repositories -&gt; New를 눌러 새 레파지토리 생성 Repository name은 꼭...","categories": ["블로그 만들기"],
+        "tags": ["Github blog","개발자 블로그 포트폴리오","Jekyll blog","Minimal Mistakes 설치"],
+        "url": "/%EB%B8%94%EB%A1%9C%EA%B7%B8%20%EB%A7%8C%EB%93%A4%EA%B8%B0/CreateABlog1/",
+        "teaser": null
+      },{
+        "title": "포스팅 하는법",
+        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 기본적인 포스팅 방법 포스팅할 글의 경우 /posts에 올려주면 됩니다. 하지만 처음에 없으므로 생성합니다. 새로운 파일 생성 _posts/ &lt;- / 를 치면 폴더 생성입니다. 파일 제목은 꼭 년도-월-일-ab-cd.md 형식으로 만들어야 합니다. Commit new file을 해주세요. 이와...","categories": ["블로그 만들기"],
+        "tags": ["Github blog 포스팅","Jekyll 포스팅 하는 법"],
+        "url": "/%EB%B8%94%EB%A1%9C%EA%B7%B8%20%EB%A7%8C%EB%93%A4%EA%B8%B0/CreateABlog2/",
+        "teaser": null
+      },{
+        "title": "지킬 블로그 로컬 환경 만들기",
+        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 설치 목록. 이번에는 터미널 활용과, 로컬 서버 생성을 하기 위한 셋업 등을 알려드릴게요. 결론적으로 아래와 같은 것들을 설치해야 합니다. Homebrew rbenv ruby (맥은 기본적으로 깔려있지만, 다른 버전 설치해야 함) jekyll, bundler Homebrew 설치 아래 링크에서...","categories": ["블로그 만들기"],
+        "tags": ["Jekyll blog 로컬 환경 만들기","homebrew 설치","rbenv 설치","루비 설치","Jekyll과 Bundler 설치"],
+        "url": "/%EB%B8%94%EB%A1%9C%EA%B7%B8%20%EB%A7%8C%EB%93%A4%EA%B8%B0/CreateABlog3/",
+        "teaser": null
+      },{
+        "title": "깃허브 블로그 로컬 호스팅 하기 (실시간 확인)",
+        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 블로그 로컬 호스팅 하기 자신의 블로그 레파지토리 폴더를 터미널로 연다. 해당 폴더 안에서 아래의 명령어 입력으로 번들을 설치해 준다. bundle install 그럼 번들이 우리의 블로그에 설치되었을 것이다. 그렇다면 아래의 명령어로 서버를 실행시켜준다. bundle exec jekyll...","categories": ["블로그 만들기"],
+        "tags": ["Jekyll blog 로컬 환경 만들기","Jekyll blog 실시간 확인"],
+        "url": "/%EB%B8%94%EB%A1%9C%EA%B7%B8%20%EB%A7%8C%EB%93%A4%EA%B8%B0/CreateABlog4/",
+        "teaser": null
+      },{
+        "title": "VS Code 설치, 확장팩 설치",
+        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 포스팅을 편리하게 하기 위한 VS code 설치 설치 아래의 링크에서 설치해 주세요. VS Code 익스텐션 설치 확장팩을 설치하는 과정입니다. 왼쪽 맨 아래 확장 메뉴에서 해당 팩들을 설치해 주세요. (저는 설치되어 있어서 제거로 나오고 있네요.) 한글...","categories": ["블로그 만들기"],
+        "tags": ["VS code 설치","Paste image 설치","VS code 한글 패치"],
+        "url": "/%EB%B8%94%EB%A1%9C%EA%B7%B8%20%EB%A7%8C%EB%93%A4%EA%B8%B0/CreateABlog5/",
+        "teaser": null
+      },{
+        "title": "매우 편리한 이미지 넣기 VS code Paste Image",
+        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 Paste Image? 공식 움짤인데..? 참 설명도 헷갈리게 해 놓았다.. Paste Image 앱은 VS Code 내에서 돌아가며, 인터넷상 존재하는 혹은 캡처한 이미지 파일을 VS Code에서 간단하게 저장 및 Markdown 자동 작성까지 해준다. (여기서 포인트는 문법 작성뿐...","categories": ["블로그 만들기"],
+        "tags": ["Paste image 사용법"],
+        "url": "/%EB%B8%94%EB%A1%9C%EA%B7%B8%20%EB%A7%8C%EB%93%A4%EA%B8%B0/CreateABlog6/",
+        "teaser": null
+      },{
+        "title": "_config,yml 수정, 블로그 정보 수정하기",
+        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 _config.yml? _config.yml은 Jekyll 블로그 생성시 만들어지는 파일로 여러가지 블로그 관련 정보를 설정 할 수 있다. 스킨 선택 minimal_mistakes_skin : 설정 하고싶은 스킨을 선택해 보자. Mint를 선택 할 경우 언어 변경 locale : “ko-KR” 할 경우...","categories": ["블로그 만들기"],
+        "tags": ["_config.yml 설정하기"],
+        "url": "/%EB%B8%94%EB%A1%9C%EA%B7%B8%20%EB%A7%8C%EB%93%A4%EA%B8%B0/CreateABlog7/",
+        "teaser": null
+      },{
+        "title": "깃허브 블로그 Disqus 댓글 기능 만들기",
+        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 Disqus Disqus란 댓글 API를 제공하는 회사이다. 보통 네이버나, 티스토리 등 블로그에는 댓글 기능이 달려 있지만, Jekyll 기반 블로그, 개인 블로그 등에 댓글 기능을 추가할 때 사용하면 된다. 회원 가입. 아래 페이지에서 회원 가입을 해주세요. Disqus...","categories": ["기능 구현하기"],
+        "tags": ["Disqus","Jekyll 블로그 댓글 기능"],
+        "url": "/%EA%B8%B0%EB%8A%A5%20%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0/ImplementingFeatures1/",
+        "teaser": null
+      },{
+        "title": "Google analytics 추가하기",
+        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 Google analytics ? 웹사이트 방문자 데이터를 수집, 분석할 수 있게 도와주는 앱이다. _config.yml 설정하기. Google analytics 첫 사용자인 경우 측정 시작 클릭 후 진행. 계정 이름, 속성 이름 설정 후 다음. 업종의 경우 자신이 원하는...","categories": ["기능 구현하기"],
+        "tags": ["Google analytics","웹 로그 분석 도구"],
+        "url": "/%EA%B8%B0%EB%8A%A5%20%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0/ImplementingFeatures2/",
+        "teaser": null
+      },{
+        "title": "Jekyll 블로그 총 정보",
+        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 Jekyll 블로그 참조 블로그 링크 사이드바 크기 및 넓이, 하이퍼텍스트 밑줄제거 Minimal-Mistakes 기반 튜토리얼 화이트 / 다크 테마 토글 적용하기 (Utterances 자동 색상 변경) 스크롤바 꾸미기 사이드바 카테고리 만들기 사이드바 카테고리 포스팅 안에 이전 글,...","categories": ["블로그 만들기"],
+        "tags": ["Jekyll 블로그 참조 링크","Jekyll 설정 파일","Jekyll 설정 폴더","Jekyll 블로그 특징"],
+        "url": "/%EB%B8%94%EB%A1%9C%EA%B7%B8%20%EB%A7%8C%EB%93%A4%EA%B8%B0/CreateABlog8/",
+        "teaser": null
+      },{
+        "title": "minimal-mistakes 기반 Utterances 댓글 적용",
+        "excerpt":"Disqus에서 Ufferances로 바꾼 이유. Disqus의 치명적인 단점. 어느샌가 몰래 덕지덕지 붙어있는 광고들.. 왠지 모르게 무겁다. 왜 나만 글자가 투명색(흰색)으로 나오는지ㅠ Disqus 내 설정을 바꿔도, Only 흰색 글자만 나온다. 결국 이러한 점 때문에 바꾸었다. (하얀색 배경으로 된 테마를 사용 못 하기 때문.) 광고 없애려면 유료… 그렇다면 장점은 없나? 구글, 페이스북, 트위터,...","categories": ["기능 구현하기"],
+        "tags": ["Utterances","댓글 기능"],
+        "url": "/%EA%B8%B0%EB%8A%A5%20%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0/ImplementingFeatures3/",
+        "teaser": null
+      },{
+        "title": "객체지향 프로그래밍과 절차적 프로그래밍",
+        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 버전 JDK: OpenJDK Runtime Environment (Zulu 8.66.0.15-CA-macos-aarch64) (build 1.8.0_352-b08) IntelliJ: IntelliJ IDEA 2022.2.3 (Community Edition) 절차적 프로그래밍과 객체지향 프로그래밍. 절차적 프로그래밍 ‘절차적 프로그래밍’이라는 한국어 번역은 오해의 여지가 크다. Procedural Programming에서 Procedural를 ‘절차적’으로 번역해버려서 마치 절차적으로...","categories": ["Basic of java"],
+        "tags": ["자바의 객체지향","객체지향 프로그래밍","절차적 프로그래밍"],
+        "url": "/basic%20of%20java/BasicOfJava13/",
+        "teaser": null
+      },{
+        "title": "객체, 클래스, 인스턴스",
+        "excerpt":"개발 환경 - 2021, 맥북 프로 M1 Pro 14인치 모델 - Ventura 13.1 베타(22C5050e) 버전 버전 JDK: OpenJDK Runtime Environment (Zulu 8.66.0.15-CA-macos-aarch64) (build 1.8.0_352-b08) IntelliJ: IntelliJ IDEA 2022.2.3 (Community Edition) 클래스, 객체 상속과 오버라이딩 오버로딩 추상클래스와 인터페이스 패키지와 제어자 객체지향 프로그래밍? ex) 자동차(프로그램)은 바퀴(객체), 엔진(객체), 핸들(객체)등을 조합하여 만든다. 그러한 바퀴,...","categories": ["Basic of java"],
+        "tags": ["객체","클래스","인스턴스"],
+        "url": "/basic%20of%20java/BasicOfJava14/",
+        "teaser": null
+      },{
+        "title": "스프링 기초",
+        "excerpt":"            개발 환경            - 2021, 맥북 프로 M1 Pro 14인치 모델             - Ventura 13.1 베타(22C5050e) 버전                버전            JDK: OpenJDK Runtime Environment (Zulu 8.66.0.15-CA-macos-aarch64) (build 1.8.0_352-b08)            IntelliJ: IntelliJ IDEA 2022.2.3 (Community Edition)    ","categories": ["Basic of spring"],
+        "tags": ["Spring 기초"],
+        "url": "/basic%20of%20spring/BasicOfSpring/",
         "teaser": null
       }]
