c2e036e613d5fba2a3b4934c80fd41ea2afa0c1d
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-11-29 16:20:01 +0900

    fix: mermaid 버그 수정

--- src/routes/Detail/hooks/useMermaidEffect.ts
+++ src/routes/Detail/hooks/useMermaidEffect.ts
@@ -1,42 +1,37 @@
-import { useEffect } from "react";
-import mermaid from "mermaid";
+import { useEffect } from "react"
+import mermaid from "mermaid"
 
 const useMermaidEffect = () => {
   useEffect(() => {
-    const initializeMermaid = async () => {
-      mermaid.initialize({
-        startOnLoad: true,
-      });
+    mermaid.initialize({
+      startOnLoad: true,
+    })
 
-      const elements: HTMLCollectionOf<Element> = await new Promise((resolve) => {
-        const checkElements = () => {
-          const foundElements = document.getElementsByClassName("language-mermaid");
-          if (foundElements.length > 0) {
-            resolve(foundElements);
-          } else {
-            setTimeout(checkElements, 100);
-          }
-        };
-        checkElements();
-      });
+    if (!document) return
+    const elements: HTMLCollectionOf<Element> =
+      document.getElementsByClassName("language-mermaid")
+
+    if (!elements) return
+
+    setTimeout(() => {
 
       for (let i = 0; i < elements.length; i++) {
         mermaid.render(
             "mermaid" + i,
             elements[i].textContent || "",
             (svgCode: string) => {
-              elements[i].innerHTML = svgCode;
+              elements[i].innerHTML = svgCode
             }
-        );
+        )
       }
-    };
+    },200)
 
-    initializeMermaid();
-  }, []);
+  }, [])
 
-  return null;
-};
+  return
+}
 
-export default useMermaidEffect;
+
+export default useMermaidEffect
 
 
